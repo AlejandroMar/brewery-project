66ade1ebe89103c9eb943b2d194ea97b8cf1351f
Alejo <motungo@hotmail.com>
2019-05-11 17:51:35 +0200

    server for production

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const chalk = require('chalk');
 const debug = require('debug')('server');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const beersRouter = require('./routes/beersRoute');
 
@@ -16,9 +17,17 @@ app.use(bodyParser.json());
 
 app.use(morgan('dev'));
 
+app.use('/api/beers', beersRouter);
+
+if (process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('../client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 4000;
-app.use('/api/beers', beersRouter);
 
 app.listen(PORT, () => {
     debug(`listening on port ${chalk.yellow(PORT)}`);
