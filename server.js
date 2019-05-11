31fcbb0c51abce8d44d6358189eb88266a11f687
Alejo <motungo@hotmail.com>
2019-05-11 18:39:46 +0200

    reinstall modules and delete eslint server dependency

--- server.js
+++ server.js
@@ -21,9 +21,9 @@ app.use('/api/beers', beersRouter);
 
 if (process.env.NODE_ENV === 'production') {
     // set static folder
-    app.use(express.static('../client/build'));
+    app.use(express.static('client/build'));
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
 
